61fbe684a1be737d1331506aa77b6c214894f6e5
Jerel Kelly <kaoskelly@gmail.com>
2020-07-13 08:17:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,6 +5,6 @@ def introduction(name)
 end
 
 
-def introduction_with_language(name and language)
+def introduction_with_language(name_and_language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
